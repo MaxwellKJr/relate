c98470afe1eb0f81a10d3f361e8099daa625148c
maxwellkjr <maxwellkjr@gmail.com>
2023-05-04 01:02:47 +0200

    Added Material3 bottom NavigationBar and FloatingActionButton

--- lib/components/navigation/navigation_bar.dart
+++ lib/components/navigation/navigation_bar.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
 
-class NavigationBarCustom extends StatefulWidget {
-  const NavigationBarCustom({super.key});
+class NavigationBarMain extends StatefulWidget {
+  const NavigationBarMain({super.key});
 
   @override
-  State<NavigationBarCustom> createState() => _NavigationBarCustomState();
+  State<NavigationBarMain> createState() => _NavigationBarMainState();
 }
 
-class _NavigationBarCustomState extends State<NavigationBarCustom> {
+class _NavigationBarMainState extends State<NavigationBarMain> {
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return
-        NavigationBar(
-          destinations: const [
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(icon: Icon(Icons.people), label: "Community"),
-            NavigationDestination(icon: Icon(Icons.message), label: "Chat"),
-            NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
-          ],
-          selectedIndex: currentPageIndex,
-          onDestinationSelected: (int index) {
-            setState(() {
-              currentPageIndex = index;
-            });
-          },
-    ),
+    return NavigationBar(
+      destinations: const [
+        NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+        NavigationDestination(icon: Icon(Icons.people), label: "Community"),
+        NavigationDestination(icon: Icon(Icons.message), label: "Chat"),
+        NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+      ],
+      selectedIndex: currentPageIndex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
+    );
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:relate/components/navigation/navigation_bar.dart';
 import 'package:relate/constants/colors.dart';
 import 'package:relate/constants/size_values.dart';
 import 'package:relate/screens/authentication/login_screen.dart';
@@ -21,35 +22,48 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            body: CustomScrollView(
-      slivers: [
-        SliverAppBar.medium(
-          leading: const Icon(Icons.menu),
-          title: const Text("Relate"),
-          actions: [
-            IconButton(
-                onPressed: signOut, icon: const Icon(Icons.logout_outlined)),
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar.medium(
+              leading: const Icon(Icons.menu),
+              title: const Text("Relate"),
+              actions: [
+                IconButton(
+                    onPressed: signOut,
+                    icon: const Icon(Icons.logout_outlined)),
+              ],
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: layoutPadding, right: layoutPadding),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Recent Issues",
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w600,
+                            color: primaryColor),
+                      )
+                    ]),
+              ),
+            ),
           ],
         ),
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.only(
-                left: layoutPadding, right: layoutPadding),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "Recent Issues",
-                    style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.w600,
-                        color: primaryColor),
-                  )
-                ]),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: primaryColor,
+          elevation: 3,
+          child: const Icon(
+            Icons.add,
+            color: whiteColor,
           ),
         ),
-      ],
-    )));
+        bottomNavigationBar: const NavigationBarMain(),
+      ),
+    );
   }
 }
