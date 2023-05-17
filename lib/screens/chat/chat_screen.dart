fe62eb3371428d32f2a5f95ecca8538eb773f1dc
ELIZABETH KAPUSA <bsc-com-01-18@unima.ac.mw>
2023-05-17 15:08:12 +0200

    chat code

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -8,10 +10,33 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+  List<types.Message> _messages = [];
+  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("ChatScreen"),
+    return Scaffold(
+      body: Chat(
+          messages: _messages,
+          onSendPressed: _handleSendPressed,
+          user: _user
+      ),
     );
+  }
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: "id",
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 }
