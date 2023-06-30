f1cb9efb0a276eb0dfd1aa9ad86b2566087f6081
MaxwellKJr <maxwellkjr@gmail.com>
2023-06-30 07:18:08 +0200

    Resolved POST Issue screen

--- lib/screens/post_issue/post_issue_screen.dart
+++ lib/screens/post_issue/post_issue_screen.dart
@@ -20,10 +20,10 @@ class PostIssueScreen extends StatefulWidget {
 class _PostIssueScreenState extends State<PostIssueScreen> {
   final _postTextController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  // final String focus = 'General';
   bool isEmpty = false;
 
   final _focusController = TextEditingController();
+  String focus = '';
 
   String imageUrl = '';
 
@@ -49,11 +49,11 @@ class _PostIssueScreenState extends State<PostIssueScreen> {
     final text = _postTextController.text;
     final currentTime = DateTime.now();
 
-    final focus = _focusController.text;
+    focus = _focusController.text;
 
-    // if (focus == '') {
-    //   focus = "general";
-    // }
+    if (focus == "") {
+      focus = "General";
+    }
 
     final post = {
       'text': text,
@@ -78,7 +78,6 @@ class _PostIssueScreenState extends State<PostIssueScreen> {
                     backgroundColor: primaryColor,
                     textColor: whiteColor,
                     fontSize: 16.0),
-                // if (Vibration.hasVibrator()) {Vibration.vibrate()}
               });
       Navigator.pop(context);
     }
@@ -90,122 +89,128 @@ class _PostIssueScreenState extends State<PostIssueScreen> {
 
     return SafeArea(
         child: Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(56.0),
-        child: Container(
-          padding: const EdgeInsets.only(right: layoutPadding - 2),
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                color: Colors.grey[300]!,
-                width: 1.0,
+            appBar: PreferredSize(
+              preferredSize: const Size.fromHeight(56.0),
+              child: Container(
+                padding: const EdgeInsets.only(right: layoutPadding - 2),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey[300]!,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+                child: AppBar(
+                  title: const Text("Share"),
+                  leading: IconButton(
+                    icon: Icon(Icons.adaptive.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  actions: [
+                    FilledButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          sendPost();
+                        }
+                      },
+                      child: const Text("Post"),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          child: AppBar(
-            title: const Text("Share"),
-            leading: IconButton(
-              icon: Icon(Icons.adaptive.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
+            body: GestureDetector(
+              onTap: () {
+                FocusScopeNode currentFocus = FocusScope.of(context);
+                if (!currentFocus.hasPrimaryFocus) {
+                  currentFocus.unfocus();
+                }
               },
-            ),
-            actions: [
-              FilledButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    sendPost();
-                  }
-                },
-                child: const Text("Share Issue"),
-              )
-            ],
-          ),
-        ),
-      ),
-      body: GestureDetector(
-        onTap: () {
-          FocusScopeNode currentFocus = FocusScope.of(context);
-          if (!currentFocus.hasPrimaryFocus) {
-            currentFocus.unfocus();
-          }
-        },
-        child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Form(
-                    key: _formKey,
-                    child: SizedBox(
-                      height: 100,
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Enter some text';
-                          }
-                          return null;
-                        },
-                        controller: _postTextController,
-                        decoration: const InputDecoration(
-                          hintText: 'Share your thoughts...',
-                          border: InputBorder.none,
-                        ),
-                        maxLines: null,
-                      ),
-                    )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Stack(
                   children: [
-                    Flexible(
-                      child: CustomDropdown(
-                        fillColor: Colors.transparent,
-                        listItemStyle: const TextStyle(color: blackColor),
-                        selectedStyle: const TextStyle(color: Colors.teal),
-                        hintText: 'Choose focus',
-                        items: const [
-                          'General',
-                          'Depression',
-                          'Addiction',
-                          'Motivation'
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Form(
+                            key: _formKey,
+                            child: SizedBox(
+                              height: 100,
+                              child: TextFormField(
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Enter some text';
+                                  }
+                                  return null;
+                                },
+                                controller: _postTextController,
+                                decoration: const InputDecoration(
+                                  hintText: 'Share your thoughts...',
+                                  border: InputBorder.none,
+                                ),
+                                maxLines: null,
+                              ),
+                            )),
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(
+                        children: [
+                          Flexible(
+                            child: CustomDropdown(
+                              fillColor: Colors.transparent,
+                              listItemStyle: const TextStyle(color: blackColor),
+                              selectedStyle:
+                                  const TextStyle(color: Colors.teal),
+                              hintText: 'Choose focus',
+                              items: const [
+                                'General',
+                                'Depression',
+                                'Addiction',
+                                'Motivation'
+                              ],
+                              controller: _focusController,
+                            ),
+                          ),
+                          OutlinedButton.icon(
+                              onPressed: () async {
+                                ImagePicker imagePicker = ImagePicker();
+                                XFile? file = await imagePicker.pickImage(
+                                    source: ImageSource.gallery);
+                                print('${file?.path}');
+
+                                if (file == null) return;
+                                String uniqueImageName = DateTime.now()
+                                    .millisecondsSinceEpoch
+                                    .toString();
+
+                                Reference referenceRoot =
+                                    FirebaseStorage.instance.ref();
+                                Reference referenceDirImages =
+                                    referenceRoot.child('images');
+
+                                Reference imageReferenceToUpload =
+                                    referenceDirImages.child(uniqueImageName);
+
+                                await imageReferenceToUpload
+                                    .putFile(File(file.path));
+                                imageUrl = await imageReferenceToUpload
+                                    .getDownloadURL();
+                              },
+                              icon: const Icon(Icons.camera_alt),
+                              label: const Text("Choose Image")),
                         ],
-                        controller: _focusController,
                       ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    OutlinedButton.icon(
-                        onPressed: () async {
-                          ImagePicker imagePicker = ImagePicker();
-                          XFile? file = await imagePicker.pickImage(
-                              source: ImageSource.gallery);
-                          print('${file?.path}');
-
-                          if (file == null) return;
-                          String uniqueImageName =
-                              DateTime.now().millisecondsSinceEpoch.toString();
-
-                          Reference referenceRoot =
-                              FirebaseStorage.instance.ref();
-                          Reference referenceDirImages =
-                              referenceRoot.child('images');
-
-                          Reference imageReferenceToUpload =
-                              referenceDirImages.child(uniqueImageName);
-
-                          await imageReferenceToUpload.putFile(File(file.path));
-                          imageUrl =
-                              await imageReferenceToUpload.getDownloadURL();
-                        },
-                        icon: const Icon(Icons.camera_alt),
-                        label: const Text("Choose Image"))
+                    )
                   ],
-                )
-              ],
-            )),
-        // bottomNavigationBar: const NavigationBarMain(),
-      ),
-    ));
+                ),
+                // bottomNavigationBar: const NavigationBarMain(),
+              ),
+            )));
   }
 }
